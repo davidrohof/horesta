7da3f7b312cf16d76cb178b12545740238858c2c
davidrohof <david.rohof@wemuda.com>
2017-08-08 21:39:08 +0200

    App base, with different pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpModule, JsonpModule } from '@angular/http';
 
+//components
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { BrowseEntities } from './components/browse-entities/browse-entities.component';
+import { AddRecipeComponent } from './components/add-recipe/add-recipe';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,11 +21,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    BrowseEntities,
+    AddRecipeComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule,
+    JsonpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,7 +37,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    BrowseEntities,
+    AddRecipeComponent
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http, Response, Headers } from '@angular/http';
+import { BrowseEntities } from '../../app/components/browse-entities/browse-entities.component';
+import { AddRecipeComponent } from '../../app/components/add-recipe/add-recipe';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,47 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  dishes = [
+  {
+    "id": 1,
+    "name": "Kylling i sovs",
+    "childs": [ "Kylling", "sovs" ]
+  }, {
+    "id": 2,
+    "name": "Thaibox",
+    "childs": [ "Thai", "Box" ]
+  }, {
+      "id": 3,
+      "name": "Stegt Flæsk",
+      "childs": [ "Flæsk", "persille" ]
+    }
+  ];
 
+  constructor(
+    public navCtrl: NavController,
+    public http: Http
+    ) {
+
+  }
+
+  search(event) {
+    let nutritionEntities = this.dishes;
+    if(event.target.value && event.target.value.length >= 3) {
+      //console.log(event.target.value);
+      this.searchArray();
+    }
+  }
+
+  searchArray() {
+
+  }
+
+  browseEntities() {
+    console.log('Opening add recipe view');
+    this.navCtrl.push(AddRecipeComponent, {
+
+    });
+    // TODO: add error handling (promise)
   }
 
 }
